530b177217a0aa5d132359ffa1c79ea21dfea036
Honey634546 <51883717+Honey634546@users.noreply.github.com>
2019-12-24 19:39:04 +0800

    Add files via upload

---
+++ Assignment1/新建文本文档..bat
@@ -0,0 +1,27 @@
+Assignment1.exe -input sierpinski_triangle.txt -points 10000 -iters 0 -size 200 -output sierpinski_triangle_0.tga
+Assignment1.exe -input sierpinski_triangle.txt -points 10000 -iters 1 -size 200 -output sierpinski_triangle_1.tga
+Assignment1.exe -input sierpinski_triangle.txt -points 10000 -iters 2 -size 200 -output sierpinski_triangle_2.tga
+Assignment1.exe -input sierpinski_triangle.txt -points 10000 -iters 3 -size 200 -output sierpinski_triangle_3.tga
+Assignment1.exe -input sierpinski_triangle.txt -points 10000 -iters 4 -size 200 -output sierpinski_triangle_4.tga
+Assignment1.exe -input sierpinski_triangle.txt -points 10000 -iters 30 -size 200 -output sierpinski_triangle.tga
+
+Assignment1.exe -input dragon.txt -points 10000 -iters 0 -size 200 -output dragon_0.tga
+Assignment1.exe -input dragon.txt -points 10000 -iters 1 -size 200 -output dragon_1.tga
+Assignment1.exe -input dragon.txt -points 10000 -iters 2 -size 200 -output dragon_2.tga
+Assignment1.exe -input dragon.txt -points 10000 -iters 3 -size 200 -output dragon_3.tga
+Assignment1.exe -input dragon.txt -points 10000 -iters 4 -size 200 -output dragon_4.tga
+Assignment1.exe -input dragon.txt -points 10000 -iters 30 -size 200 -output dragon.tga
+
+Assignment1.exe -input fern.txt -points 10000 -iters 0 -size 200 -output fern_0.tga
+Assignment1.exe -input fern.txt -points 10000 -iters 1 -size 200 -output fern_1.tga
+Assignment1.exe -input fern.txt -points 10000 -iters 2 -size 200 -output fern_2.tga
+Assignment1.exe -input fern.txt -points 10000 -iters 3 -size 200 -output fern_3.tga
+Assignment1.exe -input fern.txt -points 10000 -iters 4 -size 200 -output fern_4.tga
+Assignment1.exe -input fern.txt -points 10000 -iters 30 -size 200 -output fern.tga
+
+Assignment1.exe -input giant_x.txt -points 10000 -iters 0 -size 200 -output giant_x_0.tga
+Assignment1.exe -input giant_x.txt -points 10000 -iters 1 -size 200 -output giant_x_1.tga
+Assignment1.exe -input giant_x.txt -points 10000 -iters 2 -size 200 -output giant_x_2.tga
+Assignment1.exe -input giant_x.txt -points 10000 -iters 3 -size 200 -output giant_x_3.tga
+Assignment1.exe -input giant_x.txt -points 10000 -iters 4 -size 200 -output giant_x_4.tga
+Assignment1.exe -input giant_x.txt -points 10000 -iters 30 -size 200 -output giant_x.tga
